4e9098062023f83fd1d9761940815d6f38e99865
Colibri Shin <colibrishin@gmail.com>
2023-10-05 19:24:29 +0900

    Layer에서 foreach 대신 for 사용 (#28)

--- YamYamEngine/YamYamEngine_SOURCE/yaLayer.cpp
+++ YamYamEngine/YamYamEngine_SOURCE/yaLayer.cpp
@@ -17,33 +17,33 @@ namespace ya
 
 	void Layer::Initialize()
 	{
-		for (GameObject* gameObject : mGameObjects)
+		for(int i = 0; i < mGameObjects.size(); i++)
 		{
-			gameObject->Initialize();
+			mGameObjects[i]->Initialize();
 		}
 	}
 
 	void Layer::Update()
 	{
-		for (GameObject* gameObject : mGameObjects)
+		for(int i = 0; i < mGameObjects.size(); i++)
 		{
-			gameObject->Update();
+			mGameObjects[i]->Update();
 		}
 	}
 
 	void Layer::LateUpdate()
 	{
-		for (GameObject* gameObject : mGameObjects)
+		for(int i = 0; i < mGameObjects.size(); i++)
 		{
-			gameObject->LateUpdate();
+			mGameObjects[i]->LateUpdate();
 		}
 	}
 
 	void Layer::Render()
 	{
-		for (GameObject* gameObject : mGameObjects)
+		for(int i = 0; i < mGameObjects.size(); i++)
 		{
-			gameObject->Render();
+			mGameObjects[i]->Render();
 		}
 	}
 
