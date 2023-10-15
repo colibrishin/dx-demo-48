1a24b74a9393e06068e7856c19ffc96f79800a5d
Colibri Shin <colibrishin@gmail.com>
2023-10-15 21:34:14 +0900

    TriangleMesh: 정삼각형으로 간주

--- YamYamEngine/YamYamEngine_SOURCE_static/yaShadowMesh.hpp
+++ YamYamEngine/YamYamEngine_SOURCE_static/yaShadowMesh.hpp
@@ -12,6 +12,8 @@ namespace ya
 		~ShadowMesh() override = default;
 
 		void ChangeTopPosition(Vector3 normal) { mVertices[0].pos = normal; }
+		void ChangeLeftPosition(Vector3 normal) { mVertices[2].pos = normal; }
+		void ChangeRightPosition(Vector3 normal) { mVertices[1].pos = normal; }
 
 	protected:
 		renderer::Vertex* SetVertexData() override;
@@ -28,15 +30,15 @@ namespace ya
 	{
 		mVertices.resize(3);
 
-		mVertices[0].pos = Vector3(0.f, 0.5f, 0.f);
+		mVertices[0].pos = Vector3(0.f, sqrt(3) / 3, 0.f);
 		mVertices[0].color = Vector4(0.f, 0.f, 0.f, 0.f);
 		mVertices[0].uv = Vector2(0.5f, 0.f);
 
-		mVertices[1].pos = Vector3(0.5f, -0.5f, 0.f);
+		mVertices[1].pos = Vector3(0.5f, -sqrt(3) / 6, 0.f);
 		mVertices[1].color = Vector4(0.f, 0.f, 0.f, 0.6f);
 		mVertices[1].uv = Vector2(1.f, 1.f);
 
-		mVertices[2].pos = Vector3(-0.5f, -0.5f, 0.f);
+		mVertices[2].pos = Vector3(-0.5f, -(sqrt(3) / 6), 0.f);
 		mVertices[2].color = Vector4(0.f, 0.f, 0.f, 0.6f);
 		mVertices[2].uv = Vector2(0.f, 1.f);
 
